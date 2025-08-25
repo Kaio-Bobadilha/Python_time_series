3cd890f40176b8baf8dea9ebad02f11c6a55f614
Kaio Quevedo <kaiobobadilha@gmail.com>
2025-08-25 20:44:01 -0300

    Add perguntas

---
+++ cadeira_preto.py
@@ -0,0 +1,14 @@
+# Perguntas:
+
+''' O que é um cabeçalho de arquivo Python?
+        O cabeçalho e uma intrução para o interpretador do Python sobre como exercutar ou ler os arquivos
+    Para que ele serve?
+        O cabeçalho serve para:  
+            - Configurar a execução correta do script (via shebang).  
+            - Documentar o propósito do arquivo, facilitando a manutenção e colaboração.  
+            - Organizar dependências (importações) no início, garantindo que o código tenha acesso às funcionalidades necessárias.
+    
+    O que é uma docstring e para que ela serve?
+        Uma docstring (abreviação de documentation string) é uma string literal que aparece como a primeira instrução em um módulo, função, classe ou método. 
+        Ela é usada para explicar o que o código faz, seus parâmetros, o que ele retorna, e qualquer outra informação relevante.
+'''
